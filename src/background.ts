fcf005e3e9b931548a914ec7fa1e5d3e09cf8a04
ninoseki <zgok0079@gmail.com>
2018-02-10 13:36:24 +0900

    Shaped for release

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,22 @@
 
 function search(info, tab) {
   let query = info.linkUrl || info.selectionText
-  var url = `https://urlscan.io/search/#${query}`
+  let normalized = normalize(query)
+  let url = `https://urlscan.io/search/#${normalized}`
   chrome.tabs.create({ url: url })
+}
+
+function normalize(query) {
+  try {
+    let url = new URL(query)
+    let normalized = url.host + url.pathname + url.search + url.hash
+    if (normalized.slice(-1) == "/") {
+      normalized = normalized.slice(0, -1)
+    }
+    return normalized
+  } catch (err) {
+    return query
+  }
 }
 
 chrome.contextMenus.onClicked.addListener(search);
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        background: path.join(__dirname, 'src/background.ts'),
+        background: path.join(__dirname, 'src/background.ts')
     },
     output: {
         path: path.join(__dirname, 'dist/js'),
