106a98c3728b8d05b2c6af3142b5473710e6d52f
Manabu Niseki <manabu.niseki@gmail.com>
2018-08-03 10:06:24 +0900

    Add ONYPHE support

--- src/lib/searcher/index.ts
+++ src/lib/searcher/index.ts
@@ -2,6 +2,7 @@ export { Censys } from "./censys";
 export { DomainBigData } from "./domainbigdata";
 export { FindSubDomains } from "./findsubdomains";
 export { HybridAnalysis } from "./hybridanalysis";
+export { ONYPHE } from "./onyphe";
 export { PublicWWW } from "./publicwww";
 export { Pulsedive } from "./pulsedive";
 export { Searcher } from "./searcher";
