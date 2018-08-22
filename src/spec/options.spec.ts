e675124068285ab49913a690b87303039f6786d2
Manabu Niseki <manabu.niseki@gmail.com>
2018-08-22 15:55:33 +0900

    Add specs

--- src/spec/options.spec.ts
+++ src/spec/options.spec.ts
@@ -5,7 +5,12 @@ import * as sinon from "sinon";
 import SinonChrome = require("sinon-chrome");
 import * as root from "window-or-global";
 
-import { saveOptions, saveApiKeys, saveSearcherStates, restoreApiKeys } from "../options";
+import {
+  saveApiKeys,
+  saveSearcherStates,
+  restoreApiKeys,
+  restoreSearcherStates
+} from "../options";
 
 describe("Options script", () => {
   beforeEach(() => {
@@ -30,7 +35,7 @@ describe("Options script", () => {
     afterEach(() => {
       (root.document.getElementById as sinon.SinonStub).restore();
     });
-    it("should call chrome.storage.sync.set()", () => {
+    it("should save apiKeys via chrome.storage.sync.set()", () => {
       expect(root.chrome.storage.sync.set.notCalled).to.be.true;
       saveApiKeys();
       expect(root.chrome.storage.sync.set.withArgs({
@@ -64,7 +69,7 @@ describe("Options script", () => {
     afterEach(() => {
       (root.document.getElementById as sinon.SinonStub).restore();
     });
-    it("should call chrome.storage.sync.set()", () => {
+    it("should save searcherStates via chrome.storage.sync.set()", () => {
       expect(root.chrome.storage.sync.set.notCalled).to.be.true;
       saveSearcherStates();
       expect(root.chrome.storage.sync.set.withArgs({
@@ -92,7 +97,7 @@ describe("Options script", () => {
     afterEach(() => {
       (root.document.getElementById as sinon.SinonStub).restore();
     });
-    it("should call chrome.storage.sync.get()", async () => {
+    it("should restore via chrome.storage.sync.get()", async () => {
       root.chrome.storage.sync.get.withArgs("apiKeys").yieldsAsync({
         apiKeys: {
           urlscanApiKey: "test1",
@@ -107,6 +112,51 @@ describe("Options script", () => {
       expect(urlscanApiKey.value).to.equal("test1");
       const virusTotalApiKey = root.document.getElementById("virustotal-api-key") as HTMLInputElement;
       expect(virusTotalApiKey.value).to.equal("test2");
+    });
+  });
+  describe("#restoreSearcherStates", () => {
+    beforeEach(() => {
+      const dom = new JSDOM();
+      root.document = dom.window.document;
+
+      const searcherList = root.document.createElement('div') as HTMLElement;;
+      searcherList.id = "searcherList";
+
+      const stub = sinon.stub(root.document, "getElementById");
+      stub.withArgs("searcherList").returns(searcherList);
+      stub.withArgs("checkTemplate").returns({
+        innerHTML: `
+        <div class="field has-addons">
+          <div class="control is-expanded">
+            <div>
+              <label class="label">{{name}}</label>
+              <span>Supported types: {{supportedTypes}}</span>
+            </div>
+          </div>
+          <div class="control">
+            <input name="{{name}}" type="checkbox" {{#isEnabled}}checked="checked" {{/isEnabled}}>
+            <label>Enable</label>
+          </div>
+        </div>`
+      })
+    });
+    afterEach(() => {
+      (root.document.getElementById as sinon.SinonStub).restore();
+    });
+    it("should compose searcerList based on searcherStates via chrome.storage.sync.get()", async () => {
+      root.chrome.storage.sync.get.withArgs("searcherStates").yieldsAsync({
+        searcherStates: {
+          Censys: false,
+        }
+      });
+      expect(root.chrome.storage.sync.get.notCalled).to.be.true;
+      await restoreSearcherStates();
+      expect(root.chrome.storage.sync.get.calledOnce).to.be.true;
+      const searcherList = root.document.getElementById("searcherList") as HTMLElement;
+      const censys = searcherList.querySelector("[name=Censys]");
+      expect((censys as HTMLInputElement).checked).to.be.false;
+      const shodan = searcherList.querySelector("[name=Shodan]");
+      expect((shodan as HTMLInputElement).checked).to.be.true;
     });
   });
 });
