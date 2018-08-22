4ef9def9e746c828bc0370bbfcf5ff8123171d67
Manabu Niseki <manabu.niseki@gmail.com>
2018-08-22 15:54:46 +0900

    Remove duplicated expectations

--- src/spec/searcher/urlscan.spec.ts
+++ src/spec/searcher/urlscan.spec.ts
@@ -8,8 +8,6 @@ describe("Urlscan", () => {
     it("should return URL", () => {
       expect(urlscan.searchByURL("https://urlscan.io")).to.
         eq("https://urlscan.io/search/#%22https%3A%2F%2Furlscan.io%22");
-      expect(urlscan.searchByDomain("urlscan.io")).to.eq("https://urlscan.io/search/#urlscan.io");
-      expect(urlscan.searchByIP("1.1.1.1")).to.eq("https://urlscan.io/search/#1.1.1.1");
     });
   });
   describe("#searchByIP", () => {
