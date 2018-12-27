13cb790c2f5af1f92532f3a451a553dbfd67046d
Manabu Niseki <manabu.niseki@gmail.com>
2018-12-27 15:25:41 +0100

    refactor: replace single-quote with double-quote

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
 const webpack = require("webpack");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
+    mode: "production",
     entry: {
-        background: path.join(__dirname, 'src/background.ts'),
-        content: path.join(__dirname, 'src/content.ts'),
-        options: path.join(__dirname, 'src/options.ts'),
+        background: path.join(__dirname, "src/background.ts"),
+        content: path.join(__dirname, "src/content.ts"),
+        options: path.join(__dirname, "src/options.ts"),
     },
     output: {
-        filename: '[name].js',
-        path: path.join(__dirname, 'dist/js'),
+        filename: "[name].js",
+        path: path.join(__dirname, "dist/js"),
     },
     module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader"
-            }
-        ]
+        rules: [{
+            test: /\.tsx?$/,
+            loader: "ts-loader"
+        }]
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: [".ts", ".tsx", ".js"]
     },
     optimization: {
         splitChunks: {
-            name: 'vendor',
-            chunks: 'all',
+            name: "vendor",
+            chunks: "all",
         }
     }
 };
