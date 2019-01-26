bf5a2c9d0d0435f181e388e304d9881e741c2ac8
Manabu Niseki <manabu.niseki@gmail.com>
2019-01-27 08:24:50 +0900

    feat: add AbuseIPDB support
    implement #177

---
+++ src/lib/searcher/abuseipdb.ts
@@ -0,0 +1,18 @@
+import { buildURL } from "../url_builder";
+import { SearchableType, Searcher } from "./searcher";
+
+export class AbuseIPDB implements Searcher {
+
+  public endpoint: string;
+  public name: string;
+  public supportedTypes: SearchableType[] = ["ip"];
+
+  constructor() {
+    this.endpoint = "https://www.abuseipdb.com";
+    this.name = "AbuseIPDB";
+  }
+
+  public searchByIP(query: string) {
+    return buildURL(this.endpoint, `/check/${query}`);
+  }
+}
