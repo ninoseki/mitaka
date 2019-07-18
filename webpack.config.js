18ec826fec9474adcb8d66f7bc3ac3adcefc6d31
Manabu Niseki <manabu.niseki@gmail.com>
2019-07-18 20:57:01 +0900

    chore: update Webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,61 @@
+const { version } = require("./package.json");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtensionReloader = require("webpack-extension-reloader");
 const path = require("path");
 
-module.exports = {
-    mode: "production",
-    entry: {
-        background: path.join(__dirname, "src/background.ts"),
-        content: path.join(__dirname, "src/content.ts"),
-        options: path.join(__dirname, "src/options.ts"),
+const config = {
+  mode: process.env.NODE_ENV,
+  context: path.join(__dirname, "/src"),
+  entry: {
+    background: "./background.ts",
+    content: "./content.ts",
+    options: "./options.ts",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist/"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "all",
     },
-    output: {
-        filename: "[name].js",
-        path: path.join(__dirname, "dist/js"),
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: "ts-loader"
-        }]
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
-    },
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks: "all",
-        }
-    }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "icons", to: "icons" },
+      { from: "options/options.html", to: "options/options.html" },
+      { from: "options/css/bulma.css", to: "options/css/bulma.css" },
+      {
+        from: "manifest.json",
+        to: "manifest.json",
+        transform: content => {
+          const jsonContent = JSON.parse(content);
+          jsonContent.version = version;
+          return JSON.stringify(jsonContent, null, 2);
+        },
+      },
+    ]),
+  ],
 };
+
+if (process.env.HMR === "true") {
+  config.plugins = (config.plugins || []).concat([
+    new ExtensionReloader({
+      manifest: path.join(__dirname, "/src/manifest.json"),
+    }),
+  ]);
+}
+
+module.exports = config;
