503a79b9db4751f44920f7d1a7f301e1adf23ba4
Ԝеѕ <5124946+wesinator@users.noreply.github.com>
2019-11-07 09:09:56 -0500

    fix scumware test
    change original template

--- test/searcher/scumware.spec.ts
+++ test/searcher/scumware.spec.ts
@@ -2,8 +2,8 @@ import { expect } from "chai";
 import "mocha";
 import { Scumware } from "../../src/lib/searcher";
 
-describe("Crtsh", () => {
-  const subject = new Crtsh();
+describe("Scumware", () => {
+  const subject = new Scumware();
 
   it("should support Domain, IP, file hash type IOCs", () => {
     expect(subject.supportedTypes).to.deep.equal(["domain", "ip", "hash"]);
