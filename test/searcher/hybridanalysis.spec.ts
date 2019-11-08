1807d1b259d6b639a0ec059c15c8f77672c592ba
wesinator <5124946+wesinator@users.noreply.github.com>
2019-11-08 08:38:01 -0500

    Hybrid-Analysis - support other hashes
    fixes #315

--- test/searcher/hybridanalysis.spec.ts
+++ test/searcher/hybridanalysis.spec.ts
@@ -14,15 +14,8 @@ describe("HybridAnalysis", () => {
       const sha256 =
         "275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f";
       expect(subject.searchByHash(sha256)).to.equal(
-        `https://www.hybrid-analysis.com/sample/${sha256}`
+        `https://www.hybrid-analysis.com/search?query=${sha256}`
       );
-    });
-
-    it("should throw an error when given a not SHA256 value", () => {
-      const md5 = "44d88612fea8a8f36de82e1278abb02f";
-      expect(() => {
-        subject.searchByHash(md5);
-      }).to.throw("HybridAnalysis supports only SHA256 hash");
     });
   });
 
