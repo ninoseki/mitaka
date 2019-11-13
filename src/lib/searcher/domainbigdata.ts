97df26394f6dadbcf0bca354d0a53a1685c65a34
Ԝеѕ <5124946+wesinator@users.noreply.github.com>
2019-11-12 19:20:25 -0500

    DomainBigData - add Email, IP (#318)
    * DomainBigData - add Email, IP
    
    * update domainbigdata types
    
    * Correct domainbigdata email URL

--- src/lib/searcher/domainbigdata.ts
+++ src/lib/searcher/domainbigdata.ts
@@ -4,7 +4,7 @@ import { Searcher, SearchableType } from "../types";
 export class DomainBigData implements Searcher {
   public endpoint: string;
   public name: string;
-  public supportedTypes: SearchableType[] = ["domain"];
+  public supportedTypes: SearchableType[] = ["domain", "ip", "email"];
 
   public constructor() {
     this.endpoint = "https://domainbigdata.com";
@@ -13,5 +13,13 @@ export class DomainBigData implements Searcher {
 
   public searchByDomain(query: string): string {
     return buildURL(this.endpoint, `/${query}`);
+  }
+
+  public searchByIP(query: string): string {
+    return buildURL(this.endpoint, `/${query}`);
+  }
+
+  public searchByEmail(query: string): string {
+    return buildURL(this.endpoint, `/email/${query}`);
   }
 }
