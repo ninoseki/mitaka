441572170f852bad1a61425471ef0e4db6877ae7
Manabu Niseki <manabu.niseki@gmail.com>
2020-01-05 09:55:38 +0900

    feat: add Spyse lookup

---
+++ src/lib/searcher/spyse.ts
@@ -0,0 +1,27 @@
+import { buildURL } from "../url_builder";
+import { Searcher, SearchableType } from "../types";
+import { extractASNumber } from "../utility";
+
+export class Spyse implements Searcher {
+  public baseURL: string;
+  public name: string;
+  public supportedTypes: SearchableType[] = ["ip", "domain", "asn"];
+
+  public constructor() {
+    this.baseURL = "https://spyse.com";
+    this.name = "Spyse";
+  }
+
+  public searchByIP(query: string): string {
+    return buildURL(this.baseURL, `/target/ip/${query}`);
+  }
+
+  public searchByDomain(query: string): string {
+    return buildURL(this.baseURL, `/target/domain/${query}`);
+  }
+
+  public searchByASN(query: string): string {
+    const asn = extractASNumber(query);
+    return buildURL(this.baseURL, `/target/as/${asn}`);
+  }
+}
