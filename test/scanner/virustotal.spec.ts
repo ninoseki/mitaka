4c3a42f355f1b17346e7acb3795c087c1909d17d
Manabu Niseki <manabu.niseki@gmail.com>
2020-05-17 14:06:14 +0900

    refactor: use VT API v3

--- test/scanner/virustotal.spec.ts
+++ test/scanner/virustotal.spec.ts
@@ -13,9 +13,13 @@ describe("VirusTotal", function () {
 
   beforeEach(() => {
     moxios.install();
-    moxios.stubRequest("https://www.virustotal.com/vtapi/v2/url/scan", {
+    moxios.stubRequest("https://www.virustotal.com/api/v3/urls", {
       response: {
-        permalink: `http://www.virustotal.com/foo`,
+        data: {
+          id:
+            "u-ef8678c0f43f6142407ca89b4a376556cd4472d26b5952efa6d3821fa9fc597b-1589690619",
+          type: "analysis",
+        },
       },
       status: 200,
     });
@@ -36,12 +40,14 @@ describe("VirusTotal", function () {
   describe("#scanByURL", function () {
     it("should return a URL", async function () {
       const res = await subject.scanByURL("http://example.com");
-      expect(res).to.equal("http://www.virustotal.com/foo");
+      expect(res).to.equal(
+        "https://www.virustotal.com/gui/url/ef8678c0f43f6142407ca89b4a376556cd4472d26b5952efa6d3821fa9fc597b/details"
+      );
     });
   });
 
   context("when apiKey is undefined", function () {
-    it("should raise an error", async function () {
+    it("should raise an error", function () {
       subject.setApiKey(undefined);
 
       expect(subject.scanByURL("http://example.com")).to.be.rejectedWith(Error);
