9188d93f883e16e44cea23db59e7cbdb0fc0fed4
Manabu Niseki <manabu.niseki@gmail.com>
2020-11-14 15:25:25 +0900

    refactor: update throttle setting

--- src/content.ts
+++ src/content.ts
@@ -30,8 +30,12 @@ export async function onSelctionChange(): Promise<void> {
 if (typeof document !== "undefined") {
   document.addEventListener(
     "selectionchange",
-    throttle(async () => {
-      await onSelctionChange();
-    }, 200)
+    throttle(
+      async () => {
+        await onSelctionChange();
+      },
+      250,
+      { middle: false }
+    )
   );
 }
