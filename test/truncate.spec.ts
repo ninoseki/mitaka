ba9ed5dbeb1c02348ebb3e093364ba6dbeedc5ce
Manabu Niseki <manabu.niseki@gmail.com>
2021-02-07 09:28:38 +0900

    refactor: update max length of trauncate

--- test/truncate.spec.ts
+++ test/truncate.spec.ts
@@ -10,7 +10,7 @@ describe("truncate", function () {
     expect(truncate("12345678912")).to.eq("12345678912");
     expect(truncate("123456789123")).to.eq("123456789123");
 
-    expect(truncate("1234567891234")).to.eq("123456789...");
-    expect(truncate("1234567891234").length).to.eq(12);
+    expect(truncate("1234567891234", 12)).to.eq("123456789...");
+    expect(truncate("1234567891234", 12).length).to.eq(12);
   });
 });
