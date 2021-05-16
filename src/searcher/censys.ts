e7d0b6524c7824dd2c6d6e9d8fc0878e03ac6320
Manabu Niseki <manabu.niseki@gmail.com>
2021-05-16 16:57:02 +0900

    feat: use Censys search v2

--- src/searcher/censys.ts
+++ src/searcher/censys.ts
@@ -5,25 +5,22 @@ import { extractASNumber } from "@/utility";
 export class Censys implements Searcher {
   public baseURL: string;
   public name: string;
-  public supportedTypes: SearchableType[] = ["ip", "domain", "asn"];
+  public supportedTypes: SearchableType[] = ["ip", "asn"];
 
   public constructor() {
-    this.baseURL = "https://censys.io";
+    this.baseURL = "https://search.censys.io";
     this.name = "Censys";
   }
 
   public searchByIP(query: string): string {
-    return buildURL(this.baseURL, `/ipv4/${query}`);
-  }
-
-  public searchByDomain(query: string): string {
-    return buildURL(this.baseURL, `/domain/${query}`);
+    return buildURL(this.baseURL, `/hosts/${query}`);
   }
 
   public searchByASN(query: string): string {
     const asn = extractASNumber(query);
-    return buildURL(this.baseURL, "/ipv4", {
+    return buildURL(this.baseURL, "/search", {
       q: `autonomous_system.asn:${asn}`,
+      resource: "hosts",
     });
   }
 }
--- test/searcher/censys.spec.ts
+++ test/searcher/censys.spec.ts
@@ -8,21 +8,14 @@ describe("Censys", function () {
   const subject = new Censys();
 
   it("should support ip, domain, asn", function () {
-    expect(subject.supportedTypes).to.deep.equal(["ip", "domain", "asn"]);
+    expect(subject.supportedTypes).to.deep.equal(["ip", "asn"]);
   });
 
   describe("#searchByIP", function () {
     const ip = "1.1.1.1";
     it("should return a URL", function () {
-      expect(subject.searchByIP(ip)).to.equal(`https://censys.io/ipv4/${ip}`);
-    });
-  });
-
-  describe("#searchByDomain", function () {
-    const domain = "github.com";
-    it("should return a URL", function () {
-      expect(subject.searchByDomain(domain)).to.equal(
-        `https://censys.io/domain/${domain}`
+      expect(subject.searchByIP(ip)).to.equal(
+        `https://search.censys.io/hosts/${ip}`
       );
     });
   });
@@ -31,7 +24,7 @@ describe("Censys", function () {
     const asn = "AS13335";
     it("should return a URL", function () {
       expect(subject.searchByASN(asn)).to.equal(
-        "https://censys.io/ipv4?q=autonomous_system.asn%3A13335"
+        "https://search.censys.io/search?q=autonomous_system.asn%3A13335&resource=hosts"
       );
     });
   });
