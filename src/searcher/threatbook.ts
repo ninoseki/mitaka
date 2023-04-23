b24828b93989c08d85488fd0c45a968f00022e7c
Manabu Niseki <manabu.niseki@gmail.com>
2023-04-23 09:51:19 +0900

    feat: support ThreatBook

---
+++ src/searcher/threatbook.ts
@@ -0,0 +1,21 @@
+import type { SearchableType, Searcher } from "@/types";
+import { buildURL } from "@/utils";
+
+export class ThreatBook implements Searcher {
+  public baseURL: string;
+  public name: string;
+  public supportedTypes: SearchableType[] = ["ip", "domain"];
+
+  public constructor() {
+    this.baseURL = "https://threatbook.io";
+    this.name = "ThreatBook";
+  }
+
+  public searchByIP(query: string): string {
+    return buildURL(this.baseURL, `/ip/${query}`);
+  }
+
+  public searchByDomain(query: string): string {
+    return buildURL(this.baseURL, `/domain/${query}`);
+  }
+}
---
+++ tests/searcher/threatbook.spec.ts
@@ -0,0 +1,25 @@
+import { ThreatBook } from "@/searcher";
+
+describe("ThreatBook", function () {
+  const subject = new ThreatBook();
+
+  it("should support ip, domain and email", function () {
+    expect(subject.supportedTypes).toEqual(["ip", "domain"]);
+  });
+
+  describe("#searchByIP", function () {
+    const ip = "1.1.1.1";
+    it("should return a URL", function () {
+      expect(subject.searchByIP(ip)).toBe(`https://threatbook.io/ip/${ip}`);
+    });
+  });
+
+  describe("#searchByDomain", function () {
+    const domain = "github.com";
+    it("should return a URL", function () {
+      expect(subject.searchByDomain(domain)).toBe(
+        `https://threatbook.io/domain/${domain}`
+      );
+    });
+  });
+});
