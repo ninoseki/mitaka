f7a533143aa2aecc9f9adc3d1e46ae4e7579d2b5
Takashi Matsumoto <77025706+t-mtsmt@users.noreply.github.com>
2024-11-19 09:17:51 +0900

    Updated test

--- tests/searcher/joesandbox.spec.ts
+++ tests/searcher/joesandbox.spec.ts
@@ -11,7 +11,7 @@ describe("JoeSandbox", function () {
     const hash = "44d88612fea8a8f36de82e1278abb02f";
     it("should return a URL", function () {
       expect(subject.searchByHash(hash)._unsafeUnwrap()).toBe(
-        `https://www.joesandbox.com/search?q=${hash}`,
+        `https://www.joesandbox.com/analysis/search?q=${hash}`,
       );
     });
   });
