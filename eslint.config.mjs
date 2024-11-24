eec2830dc89779909c86549436cc37f021e94fff
Manabu Niseki <manabu.niseki@gmail.com>
2024-11-24 09:51:54 +0900

    refactor: renew ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import eslint from "@eslint/js";
 import neverthrow from "@ninoseki/eslint-plugin-neverthrow";
+import vitest from "@vitest/eslint-plugin";
 import vueTsEslintConfig from "@vue/eslint-config-typescript";
 import * as regexpPlugin from "eslint-plugin-regexp";
 import pluginVue from "eslint-plugin-vue";
 import tseslint from "typescript-eslint";
 import vueParser from "vue-eslint-parser";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 // eslint-disable-next-line no-undef
 const mode = process.env.NODE_ENV === "production" ? "error" : "warn";
@@ -17,8 +19,10 @@ export default tseslint.config(
   ...vueTsEslintConfig(),
   regexpPlugin.configs["flat/recommended"],
   {
-    plugins: { neverthrow },
+    plugins: { neverthrow, "simple-import-sort": simpleImportSort },
     rules: {
+      "simple-import-sort/imports": mode,
+      "simple-import-sort/exports": mode,
       "neverthrow/must-use-result": "error",
       "no-console": mode,
       "no-debugger": mode,
@@ -33,4 +37,13 @@ export default tseslint.config(
       },
     },
   },
+  {
+    files: ["tests/**"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  }
 );
