396335cf7ffc483252136eb213344ce67db02401
Manabu Niseki <manabu.niseki@gmail.com>
2025-10-05 17:52:30 +0900

    cI: automate publishing (#888)

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  extensionApi: "chrome",
   modules: ["@wxt-dev/module-vue"],
   manifest: {
     name: "Mitaka",
