e5fc076de9e7d461b150dae453461ecdf273c481
Manabu Niseki <manabu.niseki@gmail.com>
2025-12-27 11:17:28 +0900

    refactor: use auto-icons (#899)

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
-  modules: ["@wxt-dev/module-vue"],
+  modules: ["@wxt-dev/module-vue", "@wxt-dev/auto-icons"],
   manifest: {
     name: "Mitaka",
     permissions: ["contextMenus", "storage", "notifications"],
