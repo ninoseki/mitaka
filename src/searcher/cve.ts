fec97f8111531e468df3020570c1618d0539ef9c
Manabu Niseki <manabu.niseki@gmail.com>
2026-01-12 10:17:49 +0900

    fix: rename as CVE

--- src/searcher/cve.ts
+++ src/searcher/cve.ts
@@ -5,7 +5,7 @@ import { buildURL } from "~/utils";
 
 import { Base } from "./base";
 
-export class MITRE extends Base {
+export class CVE extends Base {
   public baseURL: string;
   public name: string;
   public supportedTypes: SearchableType[] = ["cve"];
@@ -13,7 +13,7 @@ export class MITRE extends Base {
   public constructor() {
     super();
     this.baseURL = "https://cve.org";
-    this.name = "MITRE";
+    this.name = "CVE";
   }
 
   public searchByCVE(query: string) {
